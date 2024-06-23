7f189a419d13e6e3a542fd6b4e6f64d9833c024c
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-06-23 12:41:45 +0200

    add features filtering

--- src/main/java/com/example/weatherforecast/WeatherForecastService.java
+++ src/main/java/com/example/weatherforecast/WeatherForecastService.java
@@ -4,21 +4,33 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Service
 public class WeatherForecastService {
-    private List<JsonNode> extractDailyForecast(JsonNode jsonNode) {
+    private List<ObjectNode> extractDailyForecast(JsonNode jsonNode) {
+        List<String> features = Arrays.asList(
+                "maxtemp_c",
+                "mintemp_c",
+                "avgtemp_c",
+                "maxwind_kph",
+                "totalprecip_mm",
+                "totalsnow_cm",
+                "avghumidity",
+                "avgvis_km",
+                "uv");
         JsonNode arrNode = jsonNode.findValue("forecastday");
         if (!arrNode.isArray()) return null;
         return StreamSupport
                 .stream(arrNode.spliterator(), false)
-                .map((node) -> node.findValue("day"))
+                .map((node) -> ((ObjectNode) node.findValue("day")).retain(features))
                 .collect(Collectors.toList());
     }
     public ArrayNode fetchDataFromCity(String cityName, int days) throws JsonProcessingException {
@@ -28,7 +40,7 @@ public class WeatherForecastService {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode jsonNode = mapper.readTree(res);
         ArrayNode arrayNode = mapper.createArrayNode();
-        List<JsonNode> forecastList =  extractDailyForecast(jsonNode);
+        List<ObjectNode> forecastList =  extractDailyForecast(jsonNode);
         assert forecastList != null;
         for (JsonNode forecast : forecastList) {
             arrayNode.add(forecast);
